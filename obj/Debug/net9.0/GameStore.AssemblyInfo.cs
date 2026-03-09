f25f555bcbbe75c2ea2c8b5b9a3e24370a2a098b
Székely Balázs <szbalazs06@szikszi.local>
2026-03-09 08:49:36 +0100

    1.0.4

--- obj/Debug/net9.0/GameStore.AssemblyInfo.cs
+++ obj/Debug/net9.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d34594e679ae04fde49c00ff893bf34ec8de8ec8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbb6986f6343768d42f99d848994217adf457983")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
